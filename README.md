77da4b594def9ba99bfd0b67a4806ef359f85b14
Alex Ford <alex.ford@codetunnel.com>
2015-03-16 15:49:32 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,92 @@
+# Hearthstone Log Watcher
+
+This module is simple. It takes care of the low-level monitoring of the Hearthstone log file and emits events based on what happens in the log file. Use this module if you want to build your own Hearthstone deck tracker and don't want to do the work of parsing through the nasty log file yourself.
+
+## Usage
+
+> $ npm install hearthstone-log-watcher
+
+```javascript
+var logWatcher = require('hearthstone-log-watcher');
+
+logWatcher.on('zone-change', function (data) {
+  console.log(data.cardName + ' has moved to ' + data.team + ' ' + data.zone);
+});
+
+logWatcher.start();
+```
+
+## Events
+
+The available events you can listen for are as follows:
+
+### **game-start**
+
+The `game-start` event fires at the beginning of a match when the watcher has gathered enough data from the log to determine which of the two players is the local player. It was a lot more complicated to figure that out than one might think, so this event is pretty valuable because it eliminates the guess work. Not even the hearthstats deck tracker can determine what player is the local player ;)
+
+Callback Arguments:
+
+- **players** - an array of the players in the game and what team they are on (friendly or opposing);
+
+Example player object:
+
+```javascript
+{
+  name: 'Hologrid',
+  teamId: 1,
+  team: 'FRIENDLY'
+}
+```
+
+### **game-over**
+
+The `game-over` event fires at the end of a match and includes additional data showing who won and who lost.
+
+Callback Arguments:
+
+- **players** - the same array of players from the `game-start` event except the players have an additional status property.
+
+Exmample player object:
+
+```javascript
+{
+  name: 'Hologrid',
+  teamId: 1,
+  team: 'FRIENDLY',
+  status: 'WON'
+}
+```
+
+### **zone-change**
+
+The `zone-change` event fires whenever a game entity moves from one zone to another. Most entities are cards, but heroes and hero powers are also considered game entities and will show up in these events as well. I'm working on a way to filter those out, but they don't cause any problems currently other than just being useless data most of the time.
+
+Hearthstone has 7 zones (that I'm aware of):
+
+- DECK
+- HAND
+- PLAY
+- PLAY (Hero)
+- PLAY (Hero Power)
+- WEAPON
+- SECRET
+- GRAVEYARD
+
+The "PLAY (Hero)" and "PLAY (Hero Power)" zones are pretty useless to us because the heroes and hero powers go into their respective play zones at the beginning of the game and don't usually go to the GRAVEYARD zone until the game is over. There is one exception that I'm aware of and that is Jaraxxus. Jaraxxus sends the Gul'dan hero and the Life Tap hero power to the GRAVEYARD zone when he is played, and then the Jaraxxus entity himself and his INFERNO! hero power enter the respective play zones.
+
+The other zones are pretty straightforward. Cards in your deck are in the DECK zone. Cards in your hand are in the HAND zone. Minions on the board are in the PLAY zone. Secrets and weapons are in the SECRET and WEAPON zones respectively. When writing a deck tracker UI it usually makes the most sense to consider PLAY, SECRET, and WEAPON as a single zone; that way you can show visually whether a card is in your deck, your hand, in play, or destroyed.
+
+The `zone-change` event receives an object as an argument with data that describes the event. It contains the card name, the card ID, the team the card belongs to, and the zone the card is moving to.
+
+Example zone change data object:
+
+```javascript
+{
+  cardName: 'Knife Juggler',
+  cardId: 37,
+  team: 'OPPOSING',
+  zone: 'GRAVEYARD'
+}
+```
+
+Don't be confused by the `cardId` field. The ID is not consistent across games. Rather, the card ID is an entity ID that is assigned to that specific card for the duration of that game. It is what you need in order to track a card's status as the game progresses. For example, if you have two Knife Jugglers in your deck, you need to be able to tell which one is which. The card ID is the only way to track changes to a specific card during that game.
---
+++ index.js
@@ -0,0 +1,139 @@
+var EventEmitter = require('events').EventEmitter;
+var fs = require('fs');
+var path = require('path');
+var os = require('os');
+
+var debug = require('debug');
+var log = {
+  main: debug('HLW'), 
+  gameStart: debug('HLW:game-start'),
+  zoneChange: debug('HLW:zone-change'),
+  gameOver: debug('HLW:game-over')
+};
+
+// Determine the location of the config and log files.
+var configFile, logFile;
+if (/^win/.test(os.platform())) {
+  log.main('Windows platform detected.');
+  var programFiles = 'Program Files';
+  if (/64/.test(os.arch())) {
+    programFiles += '(x86)';
+  }
+  logFile = path.join('C:', programFiles, 'Hearthstone', 'Hearthstone_Data', 'output_log.txt');
+  configFile = path.join(process.env.LOCALAPPDATA, 'Blizzard', 'Hearthstone', 'log.config');
+} else {
+  log.main('OS X platform detected.');
+  logFile = path.join(process.env.HOME, 'Library', 'Logs', 'Unity', 'Player.log');
+  configFile = path.join(process.env.HOME, 'Library', 'Preferences', 'Blizzard', 'Hearthstone', 'log.config');
+}
+log.main('config file path: %s', configFile);
+log.main('log file path: %s', logFile);
+
+// Copy local config file to the correct location.
+// We're just gonna do this every time.
+var localConfigFile = path.join(__dirname, 'log.config');
+fs.createReadStream(localConfigFile).pipe(fs.createWriteStream(configFile));
+log.main('Copied log.config file to force Hearthstone to write to its log file.');
+
+// The watcher is an event emitter so we can emit events based on what we parse in the log.
+var emitter = new EventEmitter();
+emitter.start = function () {
+
+  log.main('Log watcher started.');
+  // Begin watching the Hearthstone log file.
+  var fileSize = fs.statSync(logFile).size;
+  var players = [];
+  var gameOverCount = 0;
+  var watcher = fs.watch(logFile, function (event, filename) {
+
+    // We're only going to read the portion of the file that we have not read so far.
+    var newFileSize = fs.statSync(logFile).size;
+    var sizeDiff = newFileSize - fileSize;
+    if (sizeDiff <= 0) {
+      fileSize = newFileSize;
+      return;
+    }
+    var buffer = new Buffer(sizeDiff);
+    var fileDescriptor = fs.openSync(logFile, 'r');
+    fs.readSync(fileDescriptor, buffer, 0, sizeDiff, fileSize + 1);
+    fs.closeSync(fileDescriptor);
+    fileSize = newFileSize;
+
+    // Iterate over each line in the buffer.
+    buffer.toString().split('\n').forEach(function (line) {
+      
+      // Check if a card is changing zones.
+      var zoneChangeRegex = /name=(.*) id=(\d+).*to (FRIENDLY|OPPOSING) (.*)$/;
+      if (zoneChangeRegex.test(line)) {
+        var parts = zoneChangeRegex.exec(line);
+        var data = {
+          cardName: parts[1],
+          cardId: parseInt(parts[2]),
+          team: parts[3],
+          zone: parts[4]
+        };
+        log.zoneChange('%s moved to %s %s.', data.cardName, data.team, data.zone)
+        emitter.emit('zone-change', data);
+      }
+
+      // Check for players entering play and track their team IDs.
+      var newPlayerRegex = /Entity=(.*) tag=TEAM_ID value=(.)$/;
+      if (newPlayerRegex.test(line)) {
+        var parts = newPlayerRegex.exec(line);
+        players.push({
+          name: parts[1],
+          teamId: parseInt(parts[2])
+        });
+      }
+
+      // Look for mulligan status line that only shows for the local FRIENDLY player.
+      // Compare the ID to the team ID and set player zones appropriately.
+      var mulliganCountRegex = /id=(\d) ChoiceType=MULLIGAN Cancelable=False CountMin=0 CountMax=\d$/;
+      if (mulliganCountRegex.test(line)) {
+        var parts = mulliganCountRegex.exec(line);
+        var teamId = parseInt(parts[1]);
+        players.forEach(function (player) {
+          if (teamId === player.teamId) {
+            player.team = 'FRIENDLY';
+          } else {
+            player.team = 'OPPOSING';
+          }
+        });
+        log.gameStart('A game has started.')
+        emitter.emit('game-start', players);
+      }
+
+      // Check if the game is over.
+      var gameOverRegex = /Entity=(.*) tag=PLAYSTATE value=(LOST|WON)$/;
+      if (gameOverRegex.test(line)) {
+        var parts = gameOverRegex.exec(line);
+        // Set the status for the appropriate player.
+        players.forEach(function (player) {
+          if (player.name === parts[1]) {
+            player.status = parts[2];
+          }
+        });
+        gameOverCount++;
+        // When both players have lost, emit a game-over event.
+        if (gameOverCount === 2) {
+          log.gameOver('The current game has ended.');
+          emitter.emit('game-over', players);
+          gameOverCount = 0;
+          players = [];
+        }
+      }
+
+    });
+
+  });
+
+  emitter.stop = function () {
+    watcher.close();
+    emitter.stop = function () {};
+  };
+};
+
+emitter.stop = function () {};
+
+// Set the entire module to our emitter.
+module.exports = emitter;
